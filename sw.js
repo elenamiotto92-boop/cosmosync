488f67fcef1a9d0be97bd2c1eaf00b0299b02003
elenamiotto92-boop <elena.miotto92@gmail.com>
2026-05-23 10:40:53 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Installazione in corso...');
-});
-self.addEventListener('fetch', (e) => {
-    // Lascia passare le richieste di rete normalmente
-});
